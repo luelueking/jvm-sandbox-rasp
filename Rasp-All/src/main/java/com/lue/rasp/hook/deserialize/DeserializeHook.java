b4b5accc0a1db3df17147aa17e49ddf756812774
陈再 <2336485988@qq.com>
2023-04-18 09:59:49 +0800

    添加DeserializeHook

---
+++ Rasp-All/src/main/java/com/lue/rasp/hook/deserialize/DeserializeHook.java
@@ -0,0 +1,67 @@
+package com.lue.rasp.hook.deserialize;
+
+import com.alibaba.jvm.sandbox.api.Information;
+import com.alibaba.jvm.sandbox.api.Module;
+import com.alibaba.jvm.sandbox.api.ModuleLifecycle;
+import com.alibaba.jvm.sandbox.api.ProcessController;
+import com.alibaba.jvm.sandbox.api.listener.ext.Advice;
+import com.alibaba.jvm.sandbox.api.listener.ext.AdviceListener;
+import com.alibaba.jvm.sandbox.api.listener.ext.EventWatchBuilder;
+import com.alibaba.jvm.sandbox.api.resource.ModuleEventWatcher;
+import com.lue.rasp.context.RequestContextHolder;
+import org.kohsuke.MetaInfServices;
+
+import javax.annotation.Resource;
+import java.io.ObjectInputStream;
+
+
+@MetaInfServices(Module.class)
+@Information(id = "rasp-deserialize-hook" , author = "1ue" , version = "0.0.1")
+public class DeserializeHook implements Module, ModuleLifecycle {
+
+    @Resource
+    private ModuleEventWatcher moduleEventWatcher;
+
+    public void checkDeserialize() {
+        new EventWatchBuilder(moduleEventWatcher)
+                .onClass(ObjectInputStream.class)
+                .includeBootstrap()
+                .onBehavior("readObject")
+                .onWatch(new AdviceListener() {
+                    @Override
+                    protected void before(Advice advice) throws Throwable {
+                        System.out.println("hook到readObject方法");
+                        // TODO 上下文分析
+                        ProcessController.throwsImmediately(new RuntimeException("Block By RASP!!!"));
+                        super.before(advice);
+                    }
+                });
+    }
+
+
+    @Override
+    public void onLoad() throws Throwable {
+
+    }
+
+    @Override
+    public void onUnload() throws Throwable {
+
+    }
+
+    @Override
+    public void onActive() throws Throwable {
+
+    }
+
+    @Override
+    public void onFrozen() throws Throwable {
+
+    }
+
+    @Override
+    public void loadCompleted() {
+        checkDeserialize();
+        System.out.println("RASP的Deserialize-HOOK加载完毕！！！");
+    }
+}
