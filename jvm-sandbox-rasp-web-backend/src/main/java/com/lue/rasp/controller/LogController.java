5cd0229096cdc77d5cb36f92b17fdc4a3a45b072
陈再 <2336485988@qq.com>
2023-07-04 11:37:23 +0800

    添加日志模块

--- jvm-sandbox-rasp-web-backend/src/main/java/com/lue/rasp/controller/LogController.java
+++ jvm-sandbox-rasp-web-backend/src/main/java/com/lue/rasp/controller/LogController.java
@@ -1,2 +1,13 @@
-package com.lue.rasp.controller;public class LogController {
+package com.lue.rasp.controller;
+
+
+import io.swagger.annotations.Api;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Api("日志管理")
+@RestController
+@RequestMapping("/logs")
+public class LogController {
+
 }
